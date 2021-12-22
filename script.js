2a7f71639524a6450f670dd118354bdce257ee65
Jabril Abdilahi <66794425+JabrilAbdilahi@users.noreply.github.com>
2021-12-22 17:51:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,172 @@
+let age = 12;
+age = 13;
+
+const birthyear = 2009;
+
+const job = coder;
+//day 2 december 14 2021
+console.log(job);
+console.log(birthyear);
+console.log(age)
+const ageMustafa = now - 2023;
+console.log(age, ageMustafa);
+const firstName = 'Jabril';
+const lastName ='Abdilai';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5; 
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+// video two day2 december 14 2021
+console.log (age > ageMustafa);
+console.log(ageMustafa >=8);
+
+const isFullAge = age
+
+const now = 2021;
+
+const agejabril = now - 2009;
+const ageMustafa = now - 2013;
+
+console.log(now - 2009 > now - 2013);
+
+//day2 video 3 december 14 2021
+
+let x, y; 
+x= y = 25-10-5;
+console.log(x,y);
+const averageAge = (agejabril + ageMustafa) /2
+console.log(agejabril, ageMustafa, averageAge);
+
+const massJabril = 105;
+const heightJabril = 62.5;
+const massMustafa = 72;
+const heightMustafa = 55.5;
+
+
+
+let firstName ="jabril";
+
+console.log(firstName);
+console.log(firstName); 
+console.log(firstName);
+
+let thefamily="jabril,jazmine,mustafa,makial,noor, hamdi";
+
+console.log(thefamily);
+
+let javascriptisfun = true;
+console.log(javascriptisfun);
+
+console.log(typeof true);
+console.log(typeof javascriptisfun);
+console.log(typeof 12);
+console.log(typeof 'jabril');
+let age = 12 
+age = 12 
+console.log(age)
+// day 3 work december 15 2021 and day 4 december 18 2021
+const noorHeight = 73;
+const noorWeight = "unkown";
+
+const HamdiHeight = 71;
+const HamdiWeight = "N/A";
+
+const jazmineHeight = 63.5;
+const jazmineWeight = 108.5;
+
+console.log(noorWeight < HamdiWeight);
+console.log(noorHeight > HamdiHeight);
+
+console.log(jazmineHeight > heightJabril);
+console.log(jazmineWeight > massJabril);
+
+const makialHeight = 20;
+const makialWeight = 18;
+
+console.log (massMustafa > makialWeight);
+console.log (heightMustafa > makialHeight);
+
+//day 4 work december 19 2021
+
+const birthYEAR = 2009;
+
+let century; if (birthYear <=2000) {
+century = 20;
+} else { century =21;
+} console.log (century);
+
+
+// day 4 work december 19 2021 
+
+
+const BMIjazmine = jazmineWeight / jazmineHeight ** 2;
+const BMIjabril = massJabril / heightJabril ** 2;
+console.log(BMIjazmine, BMIjabril);
+
+IF (BMIjabril > BMIjazmine) 
+ { console.log("Jabril's bmi is higher than jazmines") }
+
+  console.log("jazmines BMI is higher than jazmine")
+
+  // day 5 december 20 2021 
+
+
+  const inputYear = '2009';
+  console.log(number (inputYear), inputYear);
+  console.log(number(inputYear)+ 16);
+
+  console(n)
+
+
+  
+
+  const jazmine = "your feet are so long it looks like dinosaurs foot ";
+
+  console.log(jazmine);
+
+  console.log(number("javrul"));
+    console.log(string(12),12);
+
+    console.log('i am' + 12 + ' years old ');
+    console.log('12' + '4' - 4);
+
+    console.log(boolean(10));
+    console.log(boolean(null));
+    console.log(boolean(8));
+    console.log(boolean(12));
+
+const money = 100000;
+    if (money) {console.log("you need a job fatass;)");} else {console.log('are you ok you just got your rich ass scammed');}
+    
+    let height = 5.2;
+    if (height) {console.log('damn!your short got to sleep'); }else {console.log('how tall are you');}
+
+    const favorite  = neegae;
+
+    if (favorite === 12) { console.log('cool! your 12 me too');
+  } else if (favorite=8) {console.log('ew 8 are you 8 and an addict to fortnite dumb smh i am an addic of mm2'); } else {console.log('favortie=im very cool');}
+
+//day 6 decemeber 22 2021 
+
+const hasDriverLiscence = false;
+const imBored = true;
+
+console.log(hasDriverLiscence && imBored);
+console.log(hasDriverL || hasGood);
+console.log(!hasDriverLiscence);
+
+const isTired = True; 
+
+
+
+if (hasDriversLiscense && imBored && imTired); {
+
+console.log('jabril is not able to drive')
+}
+
+//booleans today video 23 stoping point 
